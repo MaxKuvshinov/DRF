a254846c19956c345c24c32c7cc534d47135fc54
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2024-12-01 21:42:34 +0300

    CRUDs for course and lesson models are described in views.py

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from lms.models import Course, Lesson
+from lms.serializers import CourseSerializer, LessonSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListAPIView,CreateAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView
 
-# Create your views here.
+
+class CourseViewSet(ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+class LessonListAPIView(ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonCreateAPIView(CreateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateAPIView(UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(DestroyAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
