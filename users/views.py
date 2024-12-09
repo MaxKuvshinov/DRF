2e593fb3c28f6a1dfe6c6e7c1d1f973ebb1b6de6
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2024-12-09 19:36:53 +0300

    Filtering has been configured for local payment terminals

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import OrderingFilter
+from users.models import User, Payments
+from users.serializers import PaymentsSerializer, UserDetailSerializer
 
-# Create your views here.
+
+class PaymentsViewSet(ModelViewSet):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ("paid_course", "paid_lesson", "payment_method",)
+    ordering_fields = ("payment_date",)
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserDetailSerializer
