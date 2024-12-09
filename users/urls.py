ec33e78dded777fcedab2c35b8248b7cea949167
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2024-12-09 19:45:17 +0300

    add urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from users.apps import UsersConfig
-from django.urls import path
+from rest_framework.routers import SimpleRouter
+from users.views import UserViewSet, PaymentsViewSet
 
 app_name = UsersConfig.name
+
+router = SimpleRouter()
+router.register(r"payments", PaymentsViewSet, basename="payments")
+router.register("users", UserViewSet, basename="users")
 
 urlpatterns = [
 ]
