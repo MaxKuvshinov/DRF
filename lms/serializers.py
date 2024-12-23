b050b963df508558cdcb5db583728d32f940c30d
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2024-12-23 20:46:59 +0300

    add get_is_subscribed in serializers.py

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
-from lms.models import Course, Lesson
+from lms.models import Course, Lesson, Subscription
 from lms.validators import YouTubeValidator
 
 
@@ -8,6 +8,12 @@ class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get("request")
+        if request and request.user.is_authenticated:
+            return Subscription.objects.filter(user=request.user, course=obj).exists()
+        return False
 
 
 class LessonSerializer(ModelSerializer):
