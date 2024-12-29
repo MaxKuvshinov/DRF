890f1b6c4b4cae951a4da8c629219a4a908f0619
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2024-12-29 20:00:58 +0300

    The project is configured to work with Celery. The application is also configured to work with celery-beat to perform periodic tasks.

--- config/settings.py
+++ config/settings.py
@@ -22,10 +22,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
     "rest_framework_simplejwt",
     "django_filters",
     "drf_yasg",
+    "django_celery_beat",
+
     "users",
     "lms",
 ]
@@ -120,4 +123,14 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
-API_KEY_STRIPE = os.getenv(("API_KEY_STRIPE"))
+API_KEY_STRIPE = os.getenv("API_KEY_STRIPE")
+
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
+
+CELERY_TIMEZONE = TIME_ZONE
+
+CELERY_TASK_TRACK_STARTED = True
+
+CELERY_TASK_TIME_LIMIT = 30 * 60
